0fde7a1cfa7164346b50f32002bd49836c0b13db
JGTV <22498524+JonnygamingTv@users.noreply.github.com>
2026-05-13 22:00:43 +0200

    Add files via upload

--- WebServer/AssemblyLoader.cs
+++ WebServer/AssemblyLoader.cs
@@ -45,8 +45,30 @@ namespace WebServer
                 Console.WriteLine("Resolver load from Default.Assemblies");
                 return AssemblyLoadContext.Default.LoadFromAssemblyName(assemblyName);
             }
+            // Fallback: ./deps/
+            string depsPath = Path.Combine("deps", assemblyName.Name + ".dll");
+            if (File.Exists(libPath))
+            {
+                Console.WriteLine("Resolver load from deps dll");
+                return LoadFromAssemblyPath(Path.GetFullPath(libPath));
+            }
 
             return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            string? path = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+
+            if (path != null)
+                return LoadUnmanagedDllFromPath(path);
+
+            string local = Path.Combine("deps", unmanagedDllName + ".so");
+
+            if (File.Exists(local))
+                return LoadUnmanagedDllFromPath(Path.GetFullPath(local));
+
+            return IntPtr.Zero;
         }
     }
 }
