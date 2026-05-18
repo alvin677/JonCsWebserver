d29b423d4a35ee13d8c43975be8aea100b70f79f
JGTV <22498524+JonnygamingTv@users.noreply.github.com>
2026-05-18 13:14:30 +0200

    Add files via upload

--- WebServer/AssemblyLoader.cs
+++ WebServer/AssemblyLoader.cs
@@ -1,11 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.Loader;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebServer
 {
